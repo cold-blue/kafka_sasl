e2ce995dbf17da0a4bded1b2dcb5d3cfd3cfdcb1
YuhanLiu <yuhan.liu@intel.com>
2016-11-25 09:45:05 +0800

    first commit

---
+++ setenv.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# File Name: setenv.sh
+# Author: ma6174
+# mail: ma6174@163.com
+# Created Time: Tue 15 Nov 2016 03:50:12 PM CST
+#########################################################################
+#!/bin/bash
+JAVA_OPTS="-Djava.security.auth.login.config=/sasl/kafka_server_jaas.conf"
